ba5c1f7abbb8613d3af9f48b18b1b9263ab8acde
Angel Velazquez <angel.velazquez@stonybrook.edu>
2022-02-13 15:35:57 -0500

    ColorStack Variation

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Valetines❤️
+# ColorStack GBM: Valetine's Day Website Workshop❤️
 <p align="center">
 <img src="./preview.gif" width=500>
 </p>
 
-## What is?
-**Valentines** is a personal project to surprise my girlfriend. In diferent languages the message **I love you** will be shown 
-and if she don't want to wait the time for the next message then has a buttom to jump to the next message. Simple and effective.
+## What is this repo?
+This repo is housing a variation of Juan Gustah's **Valentines** project. In diferent languages the message **I love you** will appear. This project is being cloned and used so that our memembers can practice using git and github while making a website for their loved
+ones.
 
 ## Built With
 
@@ -16,14 +16,8 @@ and if she don't want to wait the time for the next message then has a buttom to
 
 To use it,choose a folder where it will be cloned or repository, open the / cmd terminal and use the * git * command below:
 ```
-git clone https://github.com/JuanGustah/Valetines.git
+git clone https://github.com/ColorStack-at-Stony-Brook-University/valentines-day-gbm.git
 ```
-After that,open the index.html with the browser of your choice.
-Or open in this [link](https://12-06-2020.neocities.org/)
 
 ## License
-This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/JuanGustah/Valetines/blob/master/LICENSE) file for details.
-
-## Acknowledgments
-Thanks for [manypixels](https://www.manypixels.co/). From there I found the background image.
-Thanks for you.
+This project is licensed under the MIT License - see the [LICENSE.md]
--- main.js
+++ main.js
@@ -7,16 +7,16 @@ var loveMessages=[{us:"I love You"},{br:"Eu te amo"},{jp:"わたしわ だいす
 {ir:"Is breá liom tú"},{hu:"Szeretlek"},{eslc:"Ľúbim ťa"},{esln:"Ljubim te"},{uc:"я тебе люблю"},
 {bl:"я цябе кахаю"},{se:"Волим те"},{lx:"ech hunn dech gär"},{vt:"Tôi mến bạn"},{is:"ég elska þig"},
 {su:"jag älskar dig"},{al:"ich liebe dich"},{af:"Ek het jou lief"},{gl:"Quérote"},{alb:"Unë të dua"},{tm:"Ni mel tye"},
-{gro:"I am Groot"},{mi:"ka ern to"},{yo:"Love you,I"},{co:"01001001 00100000 0110110 01101111 01110110 01100101 00100000 01111001 01101111 01110101 "}];
+{gro:"I am Groot"},{mi:"ka ern to"},{yo:"Love you, I do"},{co:"01001001 00100000 0110110 01101111 01110110 01100101 00100000 01111001 01101111 01110101 "}];
 
-var languages=[{us:"No Estados Unidos"},{br:"No Brasil"},{jp:"No Japão"},{kr:"Na Córeia"},
-{fr:"Na França"},{es:"Na Espanha"},{it:"Na Itália"},{nr:"Na Noruega"},{tl:"Na Tailândia"},{ch:"Na China"},
-{pl:"NaPolônia"},{cr:"Na Croácia"},{ho:"Na Holanda"},{bu:"Na Bulgária"},{bo:"Na Bósnia"},{ru:"Na Rússia"},
-{di:"Na Dinarmaca"},{gr:"Na Grécia"},{tch:"Na República Tcheca"},{he:"Em Isreal"},{hv:"No Havaí"},
-{ir:"Na Irlanda"},{hu:"Na Hungria"},{eslc:"Na Eslováquia"},{esln:"Na Eslovênia"},{uc:"Na Ucrânia"},
-{bl:"Na Bielo-Rússia"},{se:"Na Sérvia"},{lx:"Em Luxemburgo"},{vt:"Na Vietnã"},{is:"Na Islândia"},
-{su:"Na Suécia"},{al:"Na Alemanha"},{af:"Na África do sul"},{gl:"Na Galiza"},{alb:"Na Albânia"},{tm:"Na Terra Média"},
-{gro:"Se eu fosse o Groot"},{mi:"Se eu fosse um minion"},{yo:"Se eu fosse o Yoda"},{co:"Se eu fosse um computador"}];
+var languages=[{us:"In the United States"},{br:"In Brazil"},{jp:"In Japan"},{kr:"In Córeia"},
+{fr:"In France"},{es:"In Spain"},{it:"In Italy"},{nr:"In Norway"},{tl:"In Thailand"},{ch:"In China"},
+{pl:"In Poland"},{cr:"In Croatia"},{ho:"In Holland"},{bu:"In Bulgaria"},{bo:"In Bosnia"},{ru:"In Russia"},
+{di:"In Dinarmaca"},{gr:"In Greece"},{tch:"In the Czech Republic"},{he:"In Israel"},{hv:"In Hawaii"},
+{ir:"In Ireland"},{hu:"In Hungary"},{eslc:"In Slovakia"},{esln:"In Slovenia"},{uc:"In Ukraine"},
+{bl:"In Belarus"},{se:"In Serbia"},{lx:"In Luxembourg"},{vt:"In Vietnam"},{is:"In Iceland"},
+{su:"In Sweden"},{al:"In Germany"},{af:"In South Africa"},{gl:"In Galicia"},{alb:"In Albania"},{tm:"In Middle Earth"},
+{gro:"If I were Groot"},{mi:"If I were a minion"},{yo:"If I were Yoda"},{co:"If I were a computer"}];
 var already=[];
 
 function initPage(){
